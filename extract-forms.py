bc006e86b35c47871c851a99bfa8d253c0fde6e3
Hezekiah Akiva Bacovcin <hbacovci@gmail.com>
2017-08-14 12:28:25 -0400

    Removed old code

--- extract-forms.py
+++
@@ -1,9 +0,0 @@
-infile = open('all.txt')
-infile.readline()
-forms = {}
-for line in infile:
-    forms[line.split('\t')[0]] = ''
-
-outfile = open('forms.txt','w')
-for key in forms:
-    outfile.write(key+'.\n')
